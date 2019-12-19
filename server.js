ade463b55604d05ca86884ff9750b9dd5cda1fd2
Grace <mgracebodur@gmail.com>
2019-12-19 01:36:02 -0500

    Started with Authorization

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const POKEDEX = require('./pokedex.json')
 
 console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
+
+app.use(function validateBearerToken(req, res, next) {
+    // const bearerToken = req.get('Authorization').split(' ')[1]
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    // console.log('validate bearer token middleware')
+    // debugger
+    //move to the next middleware
+    // if (bearerToken !== apiToken) {
+    //     return res.status(401).json({ error: 'Unauthorized request'})
+    // }
+ 
+    if(!authToken || authToken.split('')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
+    next()
+})
 
 // app.use((req, res) => {
 //     res.send('Hello, world!')
