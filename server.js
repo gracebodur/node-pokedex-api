8c8099059a6c6713a243aab81d7346a8efa6c7d7
Grace <mgracebodur@gmail.com>
2019-12-19 01:48:07 -0500

    Downloaded pokedex.json file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const POKEDEX = require('./pokedex.json')
+const POKEDEX = require('./')
 
 console.log(process.env.API_TOKEN)
 
